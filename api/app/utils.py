f3be50fdc45212286ddda87a8934bf3079136c3b
ForskyOnly <foulseumsky@gmail.com>
2024-10-01 20:47:43 +0200

    dockerfile ok

--- api/app/utils.py
+++ api/app/utils.py
@@ -5,10 +5,25 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def get_model():
-    mlflow.set_tracking_uri(os.getenv("MLFLOW_TRACKING_URI"))
+    mlflow_uri = os.getenv("MLFLOW_TRACKING_URI")
     run_id = os.getenv("BEST_RUN_ID")
-    return mlflow.pyfunc.load_model(f"runs:/{run_id}/model")
-
+    print(f"MLFLOW_TRACKING_URI: {mlflow_uri}")
+    print(f"BEST_RUN_ID: {run_id}")
+    
+    mlflow.set_tracking_uri(mlflow_uri)
+    if run_id is None:
+        raise ValueError("BEST_RUN_ID n'est pas défini dans le fichier .env")
+    
+    try:
+        model_path = f"/api_ml/models/mlruns/650145881295194294/{run_id}/artifacts/model"
+        print(f"Tentative de chargement du modèle depuis : {model_path}")
+        model = mlflow.pyfunc.load_model(model_path)
+        print("Modèle chargé avec succès")
+        return model
+    except Exception as e:
+        print(f"Erreur lors du chargement du modèle : {str(e)}")
+        raise
+    
 model = None
 
 def get_model_instance():
